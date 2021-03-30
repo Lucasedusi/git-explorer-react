88139a3d47566eae9e5708fda36d695b0f01fa17
Lucasedusi <lucasmto.16@hotmail.com>
2021-03-30 17:11:06 -0300

    created pages and routes folders

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React from "react";
+
+const Dashboard: React.FC = () => {
+  return <h1>Dashboard</h1>;
+};
+
+export default Dashboard;

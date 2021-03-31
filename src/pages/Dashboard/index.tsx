1ea941701a2b9fa89b4267e28c36d1bc8269f4b8
Lucasedusi <lucasmto.16@hotmail.com>
2021-03-31 11:25:06 -0300

    styles pages

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,59 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from "react";
+import { FiChevronRight } from "react-icons/fi";
 
-import { Title } from "./styles";
+import logoImg from "../../assets/logo.svg";
+
+import { Title, Form, Repositories } from "./styles";
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore GitHub</Title>;
+  return (
+    <>
+      <img src={logoImg} />
+      <Title>Explore repositórios no GitHub</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/33089713?v=4"
+            alt="Lucas"
+          />
+          <div>
+            <strong>lucasedusi/goBarber</strong>
+            <p>Este é um repositório para a aplicação goBarber</p>
+          </div>
+          <FiChevronRight size={25} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/33089713?v=4"
+            alt="Lucas"
+          />
+          <div>
+            <strong>lucasedusi/goBarber</strong>
+            <p>Este é um repositório para a aplicação goBarber</p>
+          </div>
+          <FiChevronRight size={25} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/33089713?v=4"
+            alt="Lucas"
+          />
+          <div>
+            <strong>lucasedusi/goBarber</strong>
+            <p>Este é um repositório para a aplicação goBarber</p>
+          </div>
+          <FiChevronRight size={25} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
